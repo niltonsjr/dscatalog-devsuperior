7dbac40c3b3dc8b014d70205aef387563ecbce5c
Nilton da Silva Junior <tonsti@gmail.com>
2023-08-15 13:39:05 +0200

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,8 +18,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
   @Autowired
   private JwtTokenStore tokenStore;
 
-  private static final String[] PUBLIC = { "/oauth/token" };
-  private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
+  @Autowired
+  private Environment env;
+
+  private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+  private static final String[] OPERATOR_OR_ADMIN = {
+    "/products/**",
+    "/categories/**",
+  };
   private static final String[] ADMIN = { "/users/**" };
 
   @Override
@@ -28,17 +36,23 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
-      http
-              .authorizeRequests(requests -> requests
-                      .antMatchers(PUBLIC)
-                      .permitAll()
-                      .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN)
-                      .permitAll()
-                      .antMatchers(OPERATOR_OR_ADMIN)
-                      .hasAnyRole("OPERATOR", "ADMIN")
-                      .antMatchers(ADMIN)
-                      .hasRole("ADMIN")
-                      .anyRequest()
-                      .authenticated());
+    //H2
+    if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+      http.headers(headers -> headers.frameOptions().disable());
+    }
+
+    http.authorizeRequests(requests ->
+      requests
+        .antMatchers(PUBLIC)
+        .permitAll()
+        .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN)
+        .permitAll()
+        .antMatchers(OPERATOR_OR_ADMIN)
+        .hasAnyRole("OPERATOR", "ADMIN")
+        .antMatchers(ADMIN)
+        .hasRole("ADMIN")
+        .anyRequest()
+        .authenticated()
+    );
   }
 }
