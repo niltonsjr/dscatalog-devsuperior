2755c82203b2bf1b0826147582e232e04b87bdde
Nilton da Silva Junior <tonsti@gmail.com>
2023-08-15 13:08:46 +0200

    Token additional information

--- backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dscatalog.config;
 
+import com.devsuperior.dscatalog.components.JwtTokenEnhancer;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -16,6 +20,15 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig
   extends AuthorizationServerConfigurerAdapter {
+
+  @Value("${security.oauth2.client.client-id}")
+  private String clientId;
+
+  @Value("${security.oauth2.client.client-secret}")
+  private String clientSecret;
+
+  @Value("${jwt.duration}")
+  private Integer jwtDuration;
 
   @Autowired
   private BCryptPasswordEncoder passwordEncoder;
@@ -28,6 +41,9 @@ public class AuthorizationServerConfig
 
   @Autowired
   private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private JwtTokenEnhancer tokenEnhancer;
 
   @Override
   public void configure(AuthorizationServerSecurityConfigurer security)
@@ -42,19 +58,22 @@ public class AuthorizationServerConfig
     throws Exception {
     clients
       .inMemory()
-      .withClient("dscatalog")
-      .secret(passwordEncoder.encode("dscatalog123"))
+      .withClient(clientId)
+      .secret(passwordEncoder.encode(clientSecret))
       .scopes("read", "write")
       .authorizedGrantTypes("password")
-      .accessTokenValiditySeconds(86400);
+      .accessTokenValiditySeconds(jwtDuration);
   }
 
   @Override
   public void configure(AuthorizationServerEndpointsConfigurer endpoints)
     throws Exception {
+    TokenEnhancerChain chain = new TokenEnhancerChain();
+    chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnhancer));
     endpoints
       .authenticationManager(authenticationManager)
       .tokenStore(tokenStore)
-      .accessTokenConverter(accessTokenConverter);
+      .accessTokenConverter(accessTokenConverter)
+      .tokenEnhancer(chain);
   }
 }
